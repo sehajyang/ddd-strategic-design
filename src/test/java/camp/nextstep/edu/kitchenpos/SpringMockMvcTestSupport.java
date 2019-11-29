025463e3af7594ae532c8bf133f21bde287c166e
sehajyang <sehajyang@gmail.com>
2019-11-29 17:52:47 +0900

    test(SpringMockMvcTestSupport) : @before설정한 setUp 함수 작성

--- src/test/java/camp/nextstep/edu/kitchenpos/SpringMockMvcTestSupport.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/SpringMockMvcTestSupport.java
@@ -1,13 +1,29 @@
 package camp.nextstep.edu.kitchenpos;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @AutoConfigureMockMvc
 public class SpringMockMvcTestSupport extends SpringTestSupport{
 
     @Autowired
     protected MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .dispatchOptions(true)
+                .alwaysDo(print())
+                .build();
+    }
 }
--- src/test/java/camp/nextstep/edu/kitchenpos/controller/MenuRestControllerTest.java
+++ src/test/java/camp/nextstep/edu/kitchenpos/controller/MenuRestControllerTest.java
@@ -1,23 +1,18 @@
 package camp.nextstep.edu.kitchenpos.controller;
 
 import camp.nextstep.edu.kitchenpos.SpringMockMvcTestSupport;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
 class MenuRestControllerTest extends SpringMockMvcTestSupport {
 
+    @DisplayName("메뉴 목록을 조회한다")
     @Test
     void getMenuList() throws Exception {
         mockMvc.perform(get("/api/menus"))
-                .andDo(print())
                 .andExpect(status().isOk())
         ;
     }
